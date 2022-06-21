a02da91ff4d6c594697aa5f456caea27ef7d9df0
unknown <sebastian.rak9@gmail.com>
2022-06-21 21:57:45 +0200

    Start hacking

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import {StrictMode} from 'react';
+import ReactDOM from 'react-dom/client';
+import './style/main.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <StrictMode>
+      <div className="box">Hi</div>
+  </StrictMode>
+);
