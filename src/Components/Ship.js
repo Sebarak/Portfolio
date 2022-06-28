acefd5e3b5a324aecea91705a8a46e527353f5bf
unknown <sebastian.rak9@gmail.com>
2022-06-29 01:25:34 +0200

    Trying to create collisioh ship-asteroids

--- src/Components/Ship.js
+++ src/Components/Ship.js
@@ -1,4 +1,17 @@
+import {useEffect} from "react";
+
 const Ship = () => {
+
+    useEffect(() => {
+        const ship = document.querySelector('.spaceship').getBoundingClientRect();
+        const asteroids = document.querySelectorAll('.asteroid');
+
+        asteroids.forEach(asteroid => {
+            asteroid.getBoundingClientRect();
+            console.log(asteroid);
+        })
+    },[])
+
     return <div className="spaceship" />
 }
 
